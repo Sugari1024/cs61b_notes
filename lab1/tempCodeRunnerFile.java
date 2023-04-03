70fbfeffb9ded8b5d2170f2a911555bb5d75b45b
Sugari <2998194565@qq.com>
2023-04-03 17:53:27 +0800

    done with Collatz

---
+++ lab1/tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+    public static int nextNumber(int n) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
+    }
